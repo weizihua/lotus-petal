9644524adee09a69aae158d709f8a1d76e2e6266
S1M0N21 <lzy18565240913@gmail.com>
2020-09-08 11:38:43 +0800

    chang abi import

--- extern/sector-storage/stats.go
+++ extern/sector-storage/stats.go
@@ -3,9 +3,9 @@ package sectorstorage
 import (
 	"time"
 
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-	"github.com/filecoin-project/specs-actors/actors/abi"
 )
 
 type Task struct {
